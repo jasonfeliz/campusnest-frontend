9990a4f73be859f475f64500dd68527e023c2fe3
jasonrfeliz <jason@meetmycampus.com>
2019-02-01 15:56:06 -0500

    some bug fixes

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -146,14 +146,12 @@ const displayEditDiscussion = function(data){
     helper.displayMessage('Post updated successfully!')
  }
  const replySuccess = function(data){
-   console.log(data)
    $('#reply-modal').hide()
 	 $('#reply-form textarea').val('')
    $('#replies-list').prepend(`<li class="reply-item"><div>${data.reply.body}</div><span>posted by: ${data.reply.user.username}</span></li>`)
  }
 
 const displayReplies = function(data) {
-  console.log(data)
   let content = ""
   data.replies.forEach(function(e){
     content += `<li class="reply-item"><div>${e.body}</div><span>posted by: ${e.user.username}</span></li>`
